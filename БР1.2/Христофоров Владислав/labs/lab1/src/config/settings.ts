d268912253181875cd331688701888897a78c5e8
NovemberMoon <novsunv@gmail.com>
2026-04-18 21:57:27 +0300

    fix: update db postgres image, fix settings and switch to ts-node-dev

--- БР1.2/Христофоров Владислав/labs/lab1/src/config/settings.ts
+++ БР1.2/Христофоров Владислав/labs/lab1/src/config/settings.ts
@@ -3,7 +3,7 @@ import { env } from 'process';
 class Settings {
     // application base settings
     APP_HOST: string = env.APP_HOST || 'localhost';
-    APP_PORT: number = parseInt(env.APP_PORT) || 8000;
+    APP_PORT: number = parseInt(env.APP_PORT || '8000', 10);
     APP_PROTOCOL: string = env.APP_PROTOCOL || 'http';
     APP_CONTROLLERS_PATH: string =
         env.APP_CONTROLLERS_PATH || '/controllers/*.controller.js';
@@ -11,7 +11,7 @@ class Settings {
 
     // db connection settings
     DB_HOST = env.DB_HOST || 'localhost';
-    DB_PORT = parseInt(env.DB_PORT) || 15432;
+    DB_PORT = parseInt(env.DB_PORT || '15432', 10);
     DB_NAME = env.DB_NAME || 'maindb';
     DB_USER = env.DB_USER || 'maindb';
     DB_PASSWORD = env.DB_PASSWORD || 'maindb';
@@ -20,9 +20,11 @@ class Settings {
 
     // jwt settings
     JWT_SECRET_KEY = env.JWT_SECRET_KEY || 'secret';
-    JWT_TOKEN_TYPE = env.JWT_SECRET_KEY || 'Bearer';
-    JWT_ACCESS_TOKEN_LIFETIME: number =
-        parseInt(env.JWT_ACCESS_TOKEN_LIFETIME) || 60 * 5;
+    JWT_TOKEN_TYPE = env.JWT_TOKEN_TYPE || 'Bearer';
+    JWT_ACCESS_TOKEN_LIFETIME: number = parseInt(
+        env.JWT_ACCESS_TOKEN_LIFETIME || '300',
+        10,
+    );
 }
 
 const SETTINGS = new Settings();
