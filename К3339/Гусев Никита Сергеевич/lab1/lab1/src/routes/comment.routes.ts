b888161ff9e8aabcbaf447af87f6969dae16fe4d
Nikita Gusev <61349357+857l@users.noreply.github.com>
2026-05-11 16:08:36 +0300

    Delete К3339/Гусев Никита Сергеевич/lab1/lab1 directory

--- К3339/Гусев Никита Сергеевич/lab1/lab1/src/routes/comment.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { CommentController } from "../controllers/comment.controller"
-
-const router = Router()
-
-router.post("/", CommentController.create)
-
-export default router
