be0e10b19754e96f1672e95849e8b275b20fc935
Artsobol <artsobol05@yandex.ru>
2026-05-20 00:58:58 +0300

    Fix docker postgres. Add API Gateway

---
+++ БР1.2/Соболев Артём/labs/lab2/FitnessAPI/docker/postgres/init/01-create-databases.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE user_service;
+CREATE DATABASE media_service;
+CREATE DATABASE blog_service;
+CREATE DATABASE training_service;
+CREATE DATABASE progress_service;
+CREATE DATABASE user_training_interaction_service;
